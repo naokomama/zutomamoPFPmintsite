aa61e433bf3625171ff9f62bbd8ca0c0329d6832
naokomama <naoko.5151.nana@gmail.com>
2024-06-07 01:59:01 +0900

    スマホ用コンソールログ9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Button, Stack, Text } from '@chakra-ui/react';
 // Dynamic import for WalletConnectView
 // const WalletConnectView = dynamic(() => import('./components/wallet-connect-view'), { ssr: false });
 import WalletConnectView from './components/wallet-connect-view'
-// import VConsole from 'vconsole';
+import VConsole from 'vconsole';
 
 export default function Home() {
   // const { address, provider, chainId } = useContext(WalletContext);
@@ -28,14 +28,29 @@ export default function Home() {
   };
 
   useEffect(() => {
-    const loadVConsole = async () => {
-      const VConsole = (await import('vconsole')).default;
-      new VConsole();
-      console.log('vConsole is initialized');
-    };
+    const vConsole = new VConsole();
+    console.log("vConsole initialized");
 
-    loadVConsole();
+    // vConsoleのスタイルを設定して非表示にする
+    const vConsoleDom = document.querySelector('.vc-container') as HTMLElement;
+    if (vConsoleDom) {
+      vConsoleDom.style.display = 'none';
+    }
+
+    return () => {
+      vConsole.destroy();
+    };
   }, []);
+  
+  // useEffect(() => {
+  //   const loadVConsole = async () => {
+  //     const VConsole = (await import('vconsole')).default;
+  //     new VConsole();
+  //     console.log('vConsole is initialized');
+  //   };
+
+  //   loadVConsole();
+  // }, []);
 
   return (
     <main className="w-screen min-h-screen flex flex-col items-center">
@@ -43,10 +58,10 @@ export default function Home() {
       {/* <img src={ SUB_DIRECTRY + "zutomamo_pink_01.png" } alt="ずとまもロゴ" style={{ width: '90%', maxWidth: '50px', height: 'auto' }} /> */}
       <img src={ SUB_DIRECTRY + "zutomamologo_pink_03.png" } alt="ずとまもロゴ" style={{ textAlign: 'center', width: '100%', maxWidth: '350px', height: '50px' }} />
       </div>
-      <div className='w-full max-w-screen-md px-4' style={{ textAlign: 'center', width: '450px' }}>
+      <div className='w-full max-w-screen-md px-3' style={{ textAlign: 'center', width: '400px' }}>
         <img src={ SUB_DIRECTRY + "PFP-100.jpg" } alt="ずとまもPFP" style={{ textAlign: 'center', width: '100%', maxWidth: '450px', height: '450px' }} />
       </div>
-      <div className='w-full max-w-screen-md px-4' style={{ textAlign: 'center', width: '450px', margin: '10px' }}>
+      <div className='w-full max-w-screen-md px-3' style={{ textAlign: 'center', width: '400px', margin: '10px' }}>
         <Stack spacing={4} direction='row' align='center'>
           <Button colorScheme='cyan' variant='outline' onClick={handleOpenseaClick}>
             <img className='mr-1 icon' src= {SUB_DIRECTRY + 'opensea.png'} alt='' />
@@ -63,7 +78,7 @@ export default function Home() {
         </Stack>
       </div>
       {/* <div className='w-full max-w-screen-md px-4'> */}
-      <div style={{ width: '450px' }}>
+      <div style={{ width: '400px' }}>
         <div className='w-full flex flex-row justify-center mt-5'>
           <WalletConnectView />
         </div>
